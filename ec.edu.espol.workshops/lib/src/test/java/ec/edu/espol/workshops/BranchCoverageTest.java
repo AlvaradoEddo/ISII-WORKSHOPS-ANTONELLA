3844340592edc1bbc2d5d3c332dd96af156ecbb7
AlvaradoEddo <eaalvara@espol.edu.ec>
2022-12-22 23:48:10 -0500

    pre-desarrollo

---
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/BranchCoverageTest.java
@@ -0,0 +1,17 @@
+package ec.edu.espol.workshops;
+
+import ec.edu.espol.workshops.CarInsurance;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class BranchCoverageTest {
+	
+}
